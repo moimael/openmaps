bfb6eaa10c446e4a56e407fcd35b1acd268ec4be
Maël Lavault <mael.lavault@mailz.org>
2015-01-27 14:43:15 +0100

    Update tools to latest release
    Signed-off-by: Maël Lavault <mael.lavault@mailz.org>

--- tools/build.js
+++ tools/build.js
@@ -1,6 +1,19 @@
 ({
     baseUrl: "js/lib",
     map: { '*': { 'jquery': 'zepto' } },
+    shim: {
+        'zepto': {
+            exports: 'Zepto'
+        },
+        'leaflet-routing-machine': {
+            deps: ['leaflet'],
+            exports: 'RoutingMachine'
+        }
+    },
+    optimize: "uglify2",
+    preserveLicenseComments: false,
+    generateSourceMaps: true,
+    useStrict: true,
     dir: "../www-built",
     appDir: "../www",
     removeCombined: true,
