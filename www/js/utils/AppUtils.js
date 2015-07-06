fe5f8bffc30ec77ef333f0d4125abff77ed47d3c
Maël Lavault <mael.lavault@mailz.org>
2015-07-06 18:20:18 +0200

    Add utils conversion functions + uuid generation
    Signed-off-by: Maël Lavault <mael.lavault@mailz.org>

--- www/js/utils/AppUtils.js
+++ www/js/utils/AppUtils.js
@@ -1,3 +1,5 @@
+var uuid = require('uuid');
+
 module.exports = {
 
   isEmpty: function(obj) {
@@ -18,5 +20,34 @@ module.exports = {
     }
 
     return true;
+  },
+
+  secondsToTime: function(secs){
+      var hours = Math.floor(secs / (60 * 60));
+
+      var divisor_for_minutes = secs % (60 * 60);
+      var minutes = Math.floor(divisor_for_minutes / 60);
+
+      var divisor_for_seconds = divisor_for_minutes % 60;
+      var seconds = Math.ceil(divisor_for_seconds);
+
+      var formattedTime = (hours > 0 ? hours + ' hour' + ((hours > 1) ? 's ' : ' ') : '') +
+               (minutes > 0 ? minutes + ' minute' + ((minutes > 1) ? 's ' : ' ') : '') +
+               seconds + ' second' + ((seconds > 1) ? 's' : '');
+      return formattedTime;
+  },
+
+  metersToDistance: function(meters){
+      if (meters > 1000) {
+          var km = (meters / 1000).toFixed(1);
+          var formattedKm = km + ' km';
+          return formattedKm;
+      }
+      var formattedMeters = meters + " m";
+      return formattedMeters;
+  },
+
+  generateUUID: function(){
+    return uuid.v4();
   }
 };
